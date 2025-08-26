da50a01a4b7ecb8da82b52540ad869759a458f89
Yue Tao <yuetao.april@gmail.com>
2025-08-26 18:53:33 +0800

    Update payment_viewer_page.py

--- payment_viewer_page.py
+++ payment_viewer_page.py
@@ -12,15 +12,17 @@ class PaymentViewerPage(BasePage):
             "clear_filter_btn":(By.XPATH,"//button[text()='Clear Filter']"),
             "payment_viewer":(By.XPATH,"//h3[normalize-space()='Payment Viewer']"),
             "import_date":(By.XPATH,"//*[text()='Import Date']"),
+            "account_type":(By.XPATH,"//*[text()='Account Type']"), # April added
             "id_er_member":(By.XPATH,"//*[text()='MPF ID of ER / Member']"),
             "mpf_account_no":(By.XPATH,"//*[text()='MPF Account No']"),
             "payment_method":(By.XPATH,"//*[text()='Payment Method']"),
-            "app_ref_no":(By.XPATH,"//*[text()='Application Ref No']"),
+            # "app_ref_no":(By.XPATH,"//*[text()='Application Ref No']"), # April changed
             "end_to_end_id":(By.XPATH,"//*[text()='End to End ID']"),
             "payment_out_ref_no":(By.XPATH,"//*[text()='Payment Out Ref No']"),
             "expected_payment_issue_date":(By.XPATH,"//*[text()='Expected Payment Issue Date']"),
             "bank_payment_ex_date":(By.XPATH,"//*[text()='Bank Payment File Exported Date']"),
             "cheque_issue_date":(By.XPATH,"//*[text()='Cheque Issue Date']"),
+            "dealing_date":(By.XPATH,"//*[text()='Dealing Date']"), # April added
             "cheque_no":(By.XPATH,"//*[text()='Cheque no / e-Payment Identifier No']"),
             "tran_type":(By.XPATH,"//*[text()='Transaction Type']"),
             "bank_cd":(By.XPATH,"//*[text()='Bank Code']"),
@@ -28,15 +30,15 @@ class PaymentViewerPage(BasePage):
             "swift_cd":(By.XPATH,"//*[text()='Swift Code']"),
             "payee":(By.XPATH,"//*[text()='Payee']"),
             "payment":(By.XPATH,"//*[text()='Payment in HKD']"),
+            "target_currency":(By.XPATH,"//*[text()='Target Currency']"),
             "status":(By.XPATH,"//*[text()='Status']"),
             "reissuance":(By.XPATH,"//*[text()='Reissuance Indicator']"),
             "payment_send_out_date":(By.XPATH,"//*[text()='Payment Send Out Date']"),
             "reject_reason":(By.XPATH,"//*[text()='Reject Reason']"),
             "reject_reason_other":(By.XPATH,"//*[text()='Reject Reason (Other)']"),
             "returned_mail":(By.XPATH,"//*[text()='Returned Mail']"),
-            "target_currency":(By.XPATH,"//*[text()='Target Currency']"),
             "info":(By.XPATH,"//*[text()='Info']"),
-            "payment_out_ref":(By.XPATH,"//tr[@class='MuiTableRow-root css-1gqug66'][1]/td[@class='MuiTableCell-root MuiTableCell-body MuiTableCell-alignRight MuiTableCell-sizeSmall css-507wcy'][7]"),
+            "payment_out_ref":(By.XPATH,"//tbody/tr[1]/td[7]"), # April changed
             }
     
     def click_payment_viewer_page(self):
@@ -68,6 +70,11 @@ class PaymentViewerPage(BasePage):
         import_date = self.find_element(*self.locators["import_date"])
         return import_date
     
+    # April added
+    def verify_account_type(self):
+        account_type = self.find_element(*self.locators["account_type"])
+        return account_type
+
     def verify_id_er_member(self):
         id_er_member = self.find_element(*self.locators["id_er_member"])
         return id_er_member
@@ -80,9 +87,9 @@ class PaymentViewerPage(BasePage):
         payment_method = self.find_element(*self.locators["payment_method"])
         return payment_method
     
-    def verify_app_ref_no(self):
-        app_ref_no = self.find_element(*self.locators["app_ref_no"])
-        return app_ref_no
+    # def verify_app_ref_no(self):
+    #     app_ref_no = self.find_element(*self.locators["app_ref_no"])
+    #     return app_ref_no
     
     def verify_end_to_end_id(self):
         end_to_end_id = self.find_element(*self.locators["end_to_end_id"])
@@ -99,6 +106,11 @@ class PaymentViewerPage(BasePage):
     def verify_cheque_issue_date(self):
         cheque_issue_date = self.find_element(*self.locators["cheque_issue_date"])
         return cheque_issue_date
+    
+    # April added
+    def verify_dealing_date(self):
+        dealing_date = self.find_element(*self.locators["dealing_date"])
+        return dealing_date
     
     def verify_bank_payment_ex_date(self):
         bank_payment_ex_date = self.find_element(*self.locators["bank_payment_ex_date"])
